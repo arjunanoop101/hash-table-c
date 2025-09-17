79c332780e73186083d3fdfaa934d912083e2f0d
arjunanoop101 <arjunanoop101@gmail.com>
2025-09-17 17:42:07 +0530

    basic implementation done

--- src/hashtable.h
+++ src/hashtable.h
@@ -1,18 +1,29 @@
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+#define HT_INITIAL_BASE_SIZE 50
+#define HT_PRIME_1 151
+#define HT_PRIME_2 163
+
+// One key-value pair
 typedef struct {
-    char *key;  // key string
-    char* value; //value string
-}  ht_item;
+    char* key;
+    char* value;
+} ht_item;
 
-
-
+// Hash table structure
 typedef struct {
-    int size;  // size of hash table
-    int count; // number of spaces filled in hashtable
-    ht_item** items;  // array of ht_items
+    int base_size;   // base size (used for resizing)
+    int size;        // actual size (prime number >= base_size)
+    int count;       // number of elements stored
+    ht_item** items; // array of pointers to items
 } ht_hash_table;
 
-
-
+// API
+ht_hash_table* ht_new();
 void ht_insert(ht_hash_table* ht, const char* key, const char* value);
 char* ht_search(ht_hash_table* ht, const char* key);
-void ht_delete(ht_hash_table* h, const char* key);
+void ht_delete(ht_hash_table* ht, const char* key);
+void ht_del_hash_table(ht_hash_table* ht);
+
+#endif
---
+++ src/prime.h
@@ -0,0 +1,5 @@
+int is_prime(const int x);
+int next_prime(const int x);
+
+
+    
