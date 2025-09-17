2c9c4c4d731a80ac7cae6c651f928d02e66b85be
arjunanoop101 <arjunanoop101@gmail.com>
2025-09-17 12:26:14 +0530

    added methods

--- src/hashtable.c
+++ src/hashtable.c
@@ -39,9 +39,44 @@ void ht_del_hash_table(ht_hash_table* ht){
     free(ht);
 }
 
+
+// (Polynomial rolling hash function) -> to find the hash of a give string
+static int ht_hash(const char* s, const int a, const int m) {
+    long hash = 0;
+    const int len_s = strlen(s);
+    for (int i = 0; i < len_s; i++) {
+        hash += (long)pow(a, len_s - (i+1)) * s[i];
+        hash = hash % m;
+    }
+    return (int)hash;
+}
+
+
+// function used for double hashing for collision handling
+static int ht_get_hash(
+    const char* s, const int num_buckets, const int attempt
+) {
+    const int hash_a = ht_hash(s, HT_PRIME_1, num_buckets);
+    const int hash_b = ht_hash(s, HT_PRIME_2, num_buckets);
+    return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
+}
+
+// function used to insert a key value pair to the table
+void ht_insert(ht_hash_table* ht, const char* key, const char* value){
+    ht_item* item = ht_new_item(key,value);
+    int index = ht_get_hash(item->key,ht->size,0);
+    ht_item* cur_item = ht->items[index];
+    int i=1;
+    while(cur_item != NULL){
+        index = ht_get_hash(item->key,ht->size,i);
+        cur_item = ht->items[index];
+        i++;
+    }
+    ht->items[index] = item;
+    ht->count++;
+}
+
 // main.c
-
-
 int main() {
     ht_hash_table* ht = ht_new();
     ht_del_hash_table(ht);
